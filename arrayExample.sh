4a59f1cad1d6f152f6160667342fca57fb211aa0
Darpan Madlani <Darpan@proclink.com>
2021-11-11 19:35:20 +0530

    Initial commit

---
+++ arrayExample.sh
@@ -0,0 +1,6 @@
+cnt=0;
+fruits[((cnt++))]="Apple";
+fruits[((cnt++))]="Orange";
+fruits[((cnt++))]="Peru";
+
+echo ${fruits[1]};
---
+++ saveAllDayRecordsOfSalary.sh
@@ -0,0 +1,43 @@
+absent=0;
+present=1;
+partTime=2;
+
+function calculateWorkingHour() {
+	case $1 in
+		$absent)
+		workingHour=0;
+		;;
+
+		$present)
+		workingHour=8;
+		;;
+
+		$partTime)
+		workingHour=4;
+		;;
+	esac
+	echo $workingHour;
+}
+
+wagePerHour=20;
+totalSalary=0;
+totalWorkingHour=0
+
+day=1;
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+do
+	wHour=$( calculateWorkingHour $((RANDOM%3)) );
+	totalWorkingHour=$(($totalWorkingHour + $wHour ));
+
+	if [ $totalWorkingHour -gt 40 ]
+	then
+		totalWorkingHour=$(( $totalWorkingHour - $wHour ));
+		break;
+	fi
+
+	salary=$(( $wagePerHour * $wHour ));
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
+done
+
+echo "Employee as earned $totalSalary$ in a month (Total working hour : $totalWorkingHour)";
